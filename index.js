4d0fd5a580bdd6e9c82eec4902ad2633bc02ac5c
sdlambert <sdlamber@gmail.com>
2016-12-29 23:00:18 -0500

    Basic API calls down

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 // Simple API to grab and parse weather data on request 
 // and secure the key
 
-const http = require('http');
+const http   = require('http');
 const dotenv = require('dotenv');
-const url = require('url');
+const url    = require('url');
 const concat = require('concat-stream');
 
 dotenv.config();
@@ -19,34 +19,42 @@ const server = http.createServer((req, res) => {
 		const urlObj = url.parse(req.url, true);
 
 		if (urlObj.pathname === "/api/forecast") {
-			if("city" in urlObj.query && "state" in urlObj.query) {
-				// WE HAVE A WINNER
+			if("link" in urlObj.query) {
 				res.writeHead(200, {"Content-Type": "application/json"});
-				let forecast = getForecast(urlObj.query.city);
+				let forecast = getForecast(urlObj.query.link);
+				//console.log(forecast);
 				res.write(JSON.stringify(forecast));
 			} //else return HTML error code
 		} else if (urlObj.pathname === "/api/city") {
-			// WE HAVE A WINNER
-			console.log("citysearch!");
-
+			//console.log("citysearch!");
 			let cityResults = getCities(urlObj.query.search);
-
+			//console.log("Got cities!");
 			res.writeHead(200, {"Content-Type": "application/json"});
-			res.write(JSON.stringify(cityResults) || "");
-			
-		} // else return HTML error code
-
-	}
-	res.end();
+			//console.log(JSON.stringify(cityResults));
+			res.write(JSON.stringify(cityResults, null, '\t'));
+		} // else return HTML error code 
+		res.end();
+	} // return HTTP error code
 });
 
-function getForecast(city, state) {
+function getForecast(link) {
 	const url = "http://api.wunderground.com/api/" + process.env.API_KEY + 
-	            "forecast/q/" + state + "/" + city + ".json";
+	            "/forecast" + link + ".json";
 
-	console.log(url);
+	let results = {};
 
-	// return awesomeforecast;
+	http.get(url, (res) => {
+		res.setEncoding("utf8");
+		res.pipe(concat((data) => {
+			results = JSON.parse(data);
+
+		}));
+		res.on("error", console.error);
+	}).on("error", console.error);
+
+	//console.log(JSON.stringify(results));
+
+	return results;
 }
 
 function getCities(search) {
@@ -54,13 +62,15 @@ function getCities(search) {
 	let results = {};
 
 	http.get(url, (res) => {
-		console.log("hello!");
 		res.setEncoding("utf8");
 		res.pipe(concat((data) => {
 			results = parseCityData(JSON.parse(data));
+			console.log(JSON.stringify(results));
 		}));
 		res.on("error", console.error);
 	}).on("error", console.error);
+
+	return results;
 }
 
 function parseCityData(data) {
@@ -69,8 +79,15 @@ function parseCityData(data) {
 	data.RESULTS.forEach((city) => {
 		cityList.push({name: city.name, link: city.l});
 	});
-	return cityList;
+
+	//console.log(JSON.stringify({cityList: cityList}));
+
+	return {cityList: cityList};
 }
+
+// function parseForecastData(data) {
+
+// }
 
 server.listen(7979);
 
@@ -80,5 +97,3 @@ server.listen(7979);
 // 
 // XX = state (AK, NY, HI, MO, CA, IA, FL, etc.)
 // YYYYYYYYYY = City with underscores (i.e. San_Francisco)
-// 
-// 
